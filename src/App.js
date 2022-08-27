815eb7b792502e967f75f167ca808ccd56c79cda
Oscar Acevedo <oacevedovilches@gmail.com>
2022-08-26 20:27:47 -0400

    add document js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Swal from 'sweetalert2'
+import Input from "./components/Input";
+import ButtonValidation from './components/ButtonValidation';
 
 function App() {
+  const [name, setName] = useState("")
+  const [input, setInput] = useState("");
+  const buttonClean = () => {
+    if (name === '') {
+      Swal.fire({
+        position: 'center',
+        icon: 'warning',
+        title: 'Olvidaste Ingresar tu Nombre',
+        text: 'Vuelve y llena los campos vacios.',
+        showConfirmButton: false,
+        timer: 2500
+      })
+    }else{ 
+      Swal.fire({
+        title: 'Inicio de Sesión Correcto',
+        text: 'Disfruta nuestra Página.',
+        imageUrl: 'http://pa1.narvii.com/6876/2d206527019e23ca88a5f708e5d68a43a5ff15d1r1-289-243_00.gif',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'Custom image',
+      })
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='container'>
+      <h1>Bienvenido a la Validación</h1>
+
+      <Input setInput={setInput} setName={setName} />
+      {input === "252525" ? <ButtonValidation buttonClean={buttonClean} /> : ''}
     </div>
+    </>
   );
 }
 
